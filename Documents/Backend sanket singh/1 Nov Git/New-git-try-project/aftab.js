daec7fab4859fdc9a0707c8f1a63211060b264f4
Tarun Gehlaut <tarungerhlaut330@gmail.com>
2022-11-03 14:42:27 +0530

    Added kirk and Aftab

---
+++ Documents/Backend sanket singh/1 Nov Git/New-git-try-project/aftab.js
@@ -0,0 +1,5 @@
+consol.log("Turning Torso");
+let x = 10;
+const myName = "tarun";
+console.log("My age" , x , "My Name" , myName);
+
--- Documents/Backend sanket singh/1 Nov Git/New-git-try-project/index.js
+++ Documents/Backend sanket singh/1 Nov Git/New-git-try-project/index.js
@@ -1,2 +1,4 @@
 console.log("Hello world!!!");
 console.log("Checking Again");
+console.log("New change");
+
